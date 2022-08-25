2e7be8a5cb2c85d2af1fcf02b861b48b5917b6d4
Marvin <marvinpaz023@gmail.com>
2022-08-25 17:41:03 -0600

    Tarea 2

---
+++ Tarea_2/src/tarea_2/ClaseInicio.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea_2;
+
+/**
+ *
+ * @author MARVIN
+ */
+public class ClaseInicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Inicio_de_Sesion().setVisible(true);        // TODO code application logic here
+    }
+    
+}
